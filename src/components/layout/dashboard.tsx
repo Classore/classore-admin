f0a5ad26699e505c6d3053af59777e0b962e4f3d
Samson Okunola <smsnmicheal@gmail.com>
2024-11-16 23:48:49 +0100

    chore: update dashboard for new features

--- src/components/layout/dashboard.tsx
+++ src/components/layout/dashboard.tsx
@@ -39,55 +39,59 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
 		<div className="flex h-screen w-screen items-start overflow-hidden">
 			<aside className="flex h-full w-[285px] flex-col bg-primary-400">
 				<div className="min-h-24 w-full border-b"></div>
-				<div className="flex h-full w-full flex-col justify-between p-4">
-					{dashboard_links.map(({ label, links }) => {
-						const filtered = filterLinksByRole(links)
-						return (
-							<div key={label} className="flex w-full flex-col gap-4">
-								<p className="text-[10px] font-medium uppercase text-neutral-200">{label}</p>
-								<div className="flex w-full flex-col gap-4">
-									{filtered.map(({ href, label }) => (
-										<Link
-											key={label}
-											href={href}
-											className={`relative flex h-9 items-center gap-2 rounded-md px-3 text-sm capitalize transition-colors hover:bg-white/35 ${isActiveRoute(href) ? "border bg-white/80 text-primary-400" : "text-white"}`}>
-											{label}
-										</Link>
-									))}
+				<div className="flex h-full w-full flex-col overflow-y-scroll">
+					<div className="flex h-full min-h-[700px] w-full flex-col justify-between gap-10 p-4">
+						{dashboard_links.map(({ label, links }) => {
+							const filtered = filterLinksByRole(links)
+							return (
+								<div key={label} className="flex w-full flex-col gap-4">
+									<p className="text-[10px] font-medium uppercase text-neutral-200">{label}</p>
+									<div className="flex w-full flex-col gap-4">
+										{filtered.map(({ href, label }) => (
+											<Link
+												key={label}
+												href={href}
+												className={`relative flex h-9 items-center gap-2 rounded-md px-3 text-sm capitalize transition-colors hover:bg-white/35 ${isActiveRoute(href) ? "border bg-white/80 text-primary-400" : "text-white"}`}>
+												{label}
+											</Link>
+										))}
+									</div>
 								</div>
+							)
+						})}
+					</div>
+					<div className="grid min-h-24 w-full place-items-center border-t text-white">
+						<div className="flex h-[68px] w-full items-center gap-4 px-4">
+							<Avatar className="size-8">
+								<AvatarImage src="" alt={user?.first_name} />
+								<AvatarFallback className="font-semibold text-primary-300">
+									{getInitials(`${user?.first_name} ${user?.last_name}`)}
+								</AvatarFallback>
+							</Avatar>
+							<div className="flex flex-col">
+								<p className="text-sm capitalize text-white">{`${user?.first_name} ${user?.last_name}`}</p>
+								<p className="text-xs lowercase text-white/70">{user?.email}</p>
 							</div>
-						)
-					})}
-				</div>
-				<div className="grid min-h-24 w-full place-items-center border-t text-white">
-					<div className="flex h-[68px] w-full items-center gap-4 px-4">
-						<Avatar className="size-8">
-							<AvatarImage src="" alt={user?.first_name} />
-							<AvatarFallback>{getInitials(`${user?.first_name} ${user?.last_name}`)}</AvatarFallback>
-						</Avatar>
-						<div className="flex flex-col">
-							<p className="text-sm capitalize text-white">{`${user?.first_name} ${user?.last_name}`}</p>
-							<p className="text-xs lowercase text-white/70">{user?.email}</p>
+							<Dialog open={open} onOpenChange={setOpen}>
+								<DialogTrigger asChild>
+									<button>
+										<RiLogoutCircleRLine size={16} />
+									</button>
+								</DialogTrigger>
+								<DialogContent className="left-1/2 top-1/2 w-[400px] -translate-x-1/2 -translate-y-1/2">
+									<DialogTitle>Logout</DialogTitle>
+									<DialogDescription>Are you sure you want to logout?</DialogDescription>
+									<div className="grid w-full grid-cols-2 gap-4">
+										<Button variant="outline" className="w-full" onClick={() => setOpen(false)}>
+											Cancel
+										</Button>
+										<Button className="w-full" onClick={() => handleSignOut()}>
+											Continue
+										</Button>
+									</div>
+								</DialogContent>
+							</Dialog>
 						</div>
-						<Dialog open={open} onOpenChange={setOpen}>
-							<DialogTrigger asChild>
-								<button>
-									<RiLogoutCircleRLine size={16} />
-								</button>
-							</DialogTrigger>
-							<DialogContent className="left-1/2 top-1/2 w-[400px] -translate-x-1/2 -translate-y-1/2">
-								<DialogTitle>Logout</DialogTitle>
-								<DialogDescription>Are you sure you want to logout?</DialogDescription>
-								<div className="grid w-full grid-cols-2 gap-4">
-									<Button variant="outline" className="w-full" onClick={() => setOpen(false)}>
-										Cancel
-									</Button>
-									<Button className="w-full" onClick={() => handleSignOut()}>
-										Continue
-									</Button>
-								</div>
-							</DialogContent>
-						</Dialog>
 					</div>
 				</div>
 			</aside>
