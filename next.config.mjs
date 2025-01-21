e17be457ab22af4aa8db4a5bf17d51b63d2d14e5
Samson Okunola <smsnmichea@gmail.com>
2025-01-21 02:56:58 +0100

    chore: updated `next.config.mjs`

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	transpilePackages: ["rc-picker", "dayjs"],
+	images: {
+		remotePatterns: [
+			{ protocol: "https", hostname: "i.imgur.com" },
+			{ protocol: "https", hostname: "images.unsplash.com" },
+			{ protocol: "https", hostname: "res.cloudinary.com" },
+			{ protocol: "https", hostname: "lh3.googleusercontent.com" },
+			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
+		],
+	},
 };
 
 export default nextConfig;
