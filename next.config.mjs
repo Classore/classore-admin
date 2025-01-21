9f948650368521f7a82b536357ddff18be3bdbd4
Samson Okunola <smsnmichea@gmail.com>
2025-01-21 03:02:11 +0100

    chore: updated next.config.mjs, package.json, pnpm-lock.yaml for antd

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	transpilePackages: ["rc-picker", "dayjs"],
+	transpilePackages: [
+		"rc-picker",
+		"dayjs",
+		"rc-util",
+		"rc-picker",
+		"@babel/runtime",
+		"antd",
+		"@ant-design",
+		"rc-pagination",
+		"rc-calendar",
+		"rc-select",
+		"rc-time-picker",
+	],
 	images: {
 		remotePatterns: [
 			{ protocol: "https", hostname: "i.imgur.com" },
@@ -10,6 +22,31 @@ const nextConfig = {
 			{ protocol: "https", hostname: "lh3.googleusercontent.com" },
 			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
 		],
+	},
+	webpack: (config, { isServer }) => {
+		// Handle ES Modules
+		config.module.rules.push({
+			test: /\.m?js$/,
+			type: "javascript/auto",
+			resolve: {
+				fullySpecified: false,
+			},
+		});
+
+		// Handle node_modules that use ES modules
+		config.resolve.extensionAlias = {
+			".js": [".js", ".ts", ".tsx"],
+			".mjs": [".mjs", ".mts", ".mtsx"],
+		};
+
+		if (!isServer) {
+			config.resolve.fallback = {
+				...config.resolve.fallback,
+				module: false,
+			};
+		}
+
+		return config;
 	},
 };
 
