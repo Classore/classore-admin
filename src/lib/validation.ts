9c4b8a7f7883a00ee60587fb38f618d5c64a82f0
Samson Okunola <smsnmichea@gmail.com>
2025-03-11 19:48:41 +0100

    chore: updates

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -62,9 +62,12 @@ export const validateFile = (file: File | null, options?: ValidationOptions) => 
 	return undefined;
 };
 
-export function debounce(func: Function, wait: number) {
+export function debounce<T extends (...args: unknown[]) => unknown>(
+	func: T,
+	wait: number
+): (...args: Parameters<T>) => void {
 	let timeout: NodeJS.Timeout;
-	return function (...args: any[]) {
+	return function (...args: Parameters<T>) {
 		clearTimeout(timeout);
 		timeout = setTimeout(() => func(...args), wait);
 	};
