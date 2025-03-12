e3ab027954ec9b1acfc83d350bfc28376a8e9e5e
Omololu Babafemi <od.babafemi@gmail.com>
2025-03-12 12:03:13 +0100

    edit role button added

--- src/components/actions/index.ts
+++ src/components/actions/index.ts
@@ -2,6 +2,7 @@ export * from "./admin-actions";
 export * from "./course-actions";
 export * from "./delete-action";
 export * from "./exam-actions";
+export * from "./role-actions";
 export * from "./test-center-action";
 export * from "./test-center-section-action";
 export * from "./user-actions";
