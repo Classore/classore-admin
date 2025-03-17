5ec9eab8187f06385a11b2892bbcacca6c4620b7
Samson Okunola <smsnmichea@gmail.com>
2025-03-17 10:55:08 +0100

    chore: sequence update fix

--- src/store/z-store/chapter.ts
+++ src/store/z-store/chapter.ts
@@ -36,6 +36,7 @@ type ChapterActions = {
 	removeChapter: (sequence: number) => void;
 	addChapterName: (sequence: number, name: string) => void;
 	addChapterContent: (sequence: number, content: string) => void;
+	addChapterSequence: (chapierId: string, sequence: number) => void;
 	setChapters: (chapters: Chapter[]) => void;
 	reorderChapter: (chapterId: string, direction: "up" | "down") => void;
 	updateChapters: (chapters: Chapter[]) => void;
@@ -122,6 +123,13 @@ const chapterActions: ChapterActions = {
 		useChapterStore.setState((state) => ({
 			chapters: state.chapters.map((chapter) =>
 				chapter.sequence === sequence ? { ...chapter, name } : chapter
+			),
+		}));
+	},
+	addChapterSequence: (chapierId, sequence) => {
+		useChapterStore.setState((state) => ({
+			chapters: state.chapters.map((chapter) =>
+				chapter.id === chapierId ? { ...chapter, sequence } : chapter
 			),
 		}));
 	},
