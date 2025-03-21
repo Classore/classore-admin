155a12bd3e1574f30e4335f290699e429f7d8d68
Samson Okunola <smsnmichea@gmail.com>
2025-03-21 18:58:18 +0100

    chore: added user profile for admin

---
+++ src/components/user/change-password.tsx
@@ -0,0 +1,113 @@
+import { useMutation } from "@tanstack/react-query";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { TabPanel } from "@/components/shared";
+import { Input } from "@/components/ui/input";
+import type { HttpError } from "@/types";
+
+interface Props {
+	selected: string;
+}
+
+const initialValues = {
+	old_password: "",
+	new_password: "",
+	confirm_password: "",
+};
+
+export const ChangePassword = ({ selected }: Props) => {
+	const {} = useMutation({
+		mutationKey: ["update-password"],
+		onSuccess: () => {
+			console.log("success");
+		},
+		onError: (error: HttpError) => {
+			console.error(error);
+		},
+	});
+
+	const { errors, handleChange, handleSubmit, resetForm, touched } = useFormik({
+		initialValues,
+		validationSchema: Yup.object({
+			old_password: Yup.string()
+				.required("Password is required!")
+				.matches(
+					/^(?=.*[A-Z])(?=.*[\W])(?=.*[0-9])(?=.*[a-z]).{8,20}$/,
+					"Password must be at least 8 characters and contain at least one uppercase, lowercase, number and special character!"
+				),
+			new_password: Yup.string()
+				.required("Password is required!")
+				.matches(
+					/^(?=.*[A-Z])(?=.*[\W])(?=.*[0-9])(?=.*[a-z]).{8,20}$/,
+					"Password must be at least 8 characters and contain at least one uppercase, lowercase, number and special character!"
+				),
+			confirm_password: Yup.string().oneOf(
+				[Yup.ref("new_password"), undefined],
+				"Passwords must match"
+			),
+		}),
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
+
+	const handleResetForm = () => {
+		const values = {
+			old_password: "",
+			new_password: "",
+			confirm_password: "",
+		};
+		resetForm({ values });
+	};
+
+	return (
+		<TabPanel selected={selected} value="password">
+			<form onSubmit={handleSubmit} className="flex h-full w-full flex-col justify-between">
+				<div className="w-full space-y-4">
+					<Input
+						name="old_password"
+						label="Old Password"
+						onChange={handleChange}
+						type="password"
+						error={errors.old_password && touched.old_password ? errors.old_password : ""}
+					/>
+					<Input
+						name="new_password"
+						label="New Password"
+						onChange={handleChange}
+						type="password"
+						error={errors.new_password && touched.new_password ? errors.new_password : ""}
+					/>
+					<Input
+						name="confirm_password"
+						label="Confirm Password"
+						onChange={handleChange}
+						type="password"
+						error={errors.confirm_password && touched.confirm_password ? errors.confirm_password : ""}
+					/>
+				</div>
+				<div className="flex w-full items-center justify-between">
+					<Button className="w-fit" size="sm" type="button" variant="destructive">
+						Delete Account
+					</Button>
+					<div className="flex items-center gap-x-4">
+						<Button
+							onClick={() => handleResetForm()}
+							className="w-fit"
+							size="sm"
+							type="button"
+							variant="outline">
+							Reset Changes
+						</Button>
+						<Button className="w-fit" size="sm" type="submit">
+							Save Changes
+						</Button>
+					</div>
+				</div>
+			</form>
+		</TabPanel>
+	);
+};
---
+++ src/components/user/profile.tsx
@@ -0,0 +1,87 @@
+import { useMutation } from "@tanstack/react-query";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { TabPanel } from "@/components/shared";
+import { Input } from "@/components/ui/input";
+import type { HttpError } from "@/types";
+
+interface Props {
+	selected: string;
+}
+
+const initialValues = {
+	first_name: "",
+	last_name: "",
+	email: "",
+	phone: "",
+};
+
+export const Profile = ({ selected }: Props) => {
+	const {} = useMutation({
+		mutationKey: ["update-user"],
+		onSuccess: () => {
+			console.log("success");
+		},
+		onError: (error: HttpError) => {
+			console.error(error);
+		},
+	});
+
+	const { errors, handleChange, handleSubmit, touched } = useFormik({
+		initialValues,
+		validationSchema: Yup.object({}),
+		onSubmit: (values) => {
+			console.log(values);
+		},
+	});
+
+	return (
+		<TabPanel selected={selected} value="profile">
+			<form onSubmit={handleSubmit} className="flex h-full w-full flex-col justify-between">
+				<div className="w-full space-y-4">
+					<Input
+						name="first_name"
+						label="First Name"
+						onChange={handleChange}
+						error={errors.first_name && touched.first_name ? errors.first_name : ""}
+					/>
+					<Input
+						name="last_name"
+						label="Last Name"
+						onChange={handleChange}
+						error={errors.last_name && touched.last_name ? errors.last_name : ""}
+					/>
+					<Input
+						name="email"
+						label="Email"
+						type="email"
+						onChange={handleChange}
+						error={errors.email && touched.email ? errors.email : ""}
+					/>
+					<Input
+						name="phone"
+						label="Phone"
+						onChange={handleChange}
+						error={errors.phone && touched.phone ? errors.phone : ""}
+					/>
+				</div>
+				<div className="flex w-full items-center justify-between">
+					<Button className="w-fit" size="sm" type="button" variant="destructive">
+						Delete Account
+					</Button>
+					<div className="flex items-center gap-x-4">
+						<Button className="w-fit" size="sm" type="button" variant="outline">
+							Reset Changes
+						</Button>
+						<Button className="w-fit" size="sm" type="submit">
+							Save Changes
+						</Button>
+					</div>
+				</div>
+			</form>
+		</TabPanel>
+	);
+};
