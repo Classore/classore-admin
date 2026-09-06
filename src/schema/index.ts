612fcafc2370b752a14ed58df8c9264d3a229ac2
Gabriel Samuel Aina <gabriel.jpdesigns@gmail.com>
2026-09-06 20:11:52 +0100

    fix(admin): remove password complexity validation on sign in

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -16,10 +16,5 @@ export const createAdminSchema = Yup.object({
 
 export const signinSchema = Yup.object({
 	email: Yup.string().email("Please enter a valid email!").required("Your email is required!"),
-	password: Yup.string()
-		.required("Password is required!")
-		.matches(
-			/^(?=.*[A-Z])(?=.*[\W])(?=.*[0-9])(?=.*[a-z]).{8,20}$/,
-			"Password must be at least 8 characters and contain at least one uppercase, lowercase, number and special character!"
-		),
+	password: Yup.string().required("Password is required!"),
 });
